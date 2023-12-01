09bd7e29066e0fc7e209e2c66c905b737782c216
rsssrj <sarveshjayaraman@ucsb.edu>
2023-11-30 23:33:38 -0800

    addding physics

--- npc.cpp
+++ npc.cpp
@@ -5,16 +5,28 @@
 void NPC::moveRandomly(int deltaTime) {
     moveTimer -= deltaTime;
 
-    if (moveTimer <=0) {
+    if (moveTimer <= 0) {
         randomAngle = static_cast<float>(rand() % 360);
 
         // Calculate the movement components based on the random angle
         moveTimer = moveDuration;
     }
+    
+    float windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+    float windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+    if (x < -windowWidth / 2.0f || x > windowWidth / 2.0f) {
+        randomAngle = 180.0f - randomAngle;  // Reverse the angle to bounce back
+    }
+
+    if (y < -windowHeight / 2.0f || y > windowHeight / 2.0f) {
+        randomAngle = -randomAngle;  // Reverse the angle to bounce back
+    }
+
 
     float deltaX = velocity * cosf(randomAngle);
     float deltaY = velocity * sinf(randomAngle);
 
-    // Move the NPC
+    // Update the NPC's position
     move(deltaX, deltaY, deltaTime);
 }
