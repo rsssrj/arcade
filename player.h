a5f80c7a1fc41d206a970f52526454f4da5c1ed6
rohanpreetam <124843484+rohanpreetam@users.noreply.github.com>
2023-12-08 10:12:17 -0800

    Update player.h

--- player.h
+++ player.h
@@ -2,13 +2,26 @@
 #define PLAYER_H
 
 #include "blob.h"
+#include "npc.h"  // Include the header file for NPC
+
+// Declare the extern variables in the header
+extern std::vector<NPC> npcs;
+extern int elapsedTime;
 
 class player : public blob {
 public:
-    player(float startX, float startY, float startVelocity) : blob(startX, startY, startVelocity) {}
+    player(float startX, float startY, float startVelocity, float startSize)
+        : blob(startX, startY, startVelocity, startSize) {}
 
     // Function to handle player movement based on mouse input
     void moveMouse(int x, int y);
+    void addBlob(float size);
+    bool isGameOver();
+    void setGameOver(bool x);
+    void setSize(float x);
+private: 
+    bool gameOver = false;
 };
 
 #endif // PLAYER_H
+
