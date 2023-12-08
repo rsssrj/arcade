e76a0c7570628fec8314547fe90b928ae8abe351
rohanpreetam <124843484+rohanpreetam@users.noreply.github.com>
2023-12-08 10:11:38 -0800

    Update npc.h

--- npc.h
+++ npc.h
@@ -1,23 +1,25 @@
 #ifndef NPC_H
 #define NPC_H
 
+class player;  // Forward declaration
+
 #include "blob.h"
 
 class NPC : public blob {
 private:
-    int moveTimer; // Timer to control how long the NPC moves in a certain direction
-    int moveDuration; // How long the NPC should move in a certain direction (in milliseconds)
+    int moveTimer;
+    int moveDuration;
     float randomAngle;
 
 public:
-    NPC(float startX, float startY, float startVelocity) 
-    : blob(startX, startY, startVelocity) {
-        moveDuration = rand() % 17000 + 3000;
+    NPC(float startX, float startY, float startVelocity, float startSize)
+        : blob(startX, startY, startVelocity, startSize) {
+        moveDuration = rand() % 12000 + 2000;
         moveTimer = 0;
     }
 
-    // Function to make NPC move randomly
-    void moveRandomly(int deltaTime);
+    void moveRandomly(player& playerBlob, int deltaTime);
 };
 
 #endif // NPC_H
+
